98e9278702001a16ef744b60e0c63796f19bf975
TheRoamer-2901 <dangkhoa29012002@gmail.com>
2023-07-21 13:56:08 +0700

    git add update popup direction on hover

--- umap-web/src/redux/store.ts
+++ umap-web/src/redux/store.ts
@@ -1,13 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+<<<<<<< HEAD
 import  routingReducer from "./slices/routingSlice";
 import  searchReducer from "./slices/searchSlice";
 import loadingReducer from "./slices/loadingSlice";
+=======
+import routingReducer from "./slices/routingSlice";
+import searchReducer from "./slices/searchSlice";
+import popupReducer from "./slices/popupSlice"
+>>>>>>> 7334abb (git add update popup direction on hover)
 
 export const store = configureStore({
     reducer: {
         routing: routingReducer,
         search: searchReducer,
+<<<<<<< HEAD
         loading: loadingReducer
+=======
+        popup: popupReducer
+>>>>>>> 7334abb (git add update popup direction on hover)
     }
 })
 
