dad4a935ddb33d83cc0f2397ce18ba4070fdc3cb
lostmymind-18 <supercool1170@gmail.com>
2023-07-25 06:50:21 +0700

    List address done

--- umap-web/src/components/MapTools/MapInteraction/FiterListAddresses/FilterListAddresses.tsx
+++ umap-web/src/components/MapTools/MapInteraction/FiterListAddresses/FilterListAddresses.tsx
@@ -8,9 +8,11 @@ interface FilterListAddressesProps{
 }
 
 export default function FilterListAddresses(props:FilterListAddressesProps){
-
     props.addressList.type = "cafe";
-    props.addressList.list = ["Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng"];
+    props.addressList.list = ["Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học",
+    "Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học",
+    "Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học",
+    "Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học"];
 
     return (
         props.addressList?.list?.length > 0 &&
@@ -18,13 +20,13 @@ export default function FilterListAddresses(props:FilterListAddressesProps){
                 initial={{ scale: 0, opacity: 0.5 }}
                 animate={{ scale: 1, opacity: 1}}
                 transition={{ type: "spring", stiffness: 100 }}
-                className={"w-fit h-fit bg-white absolute rounded-md shadow-xl drop-shadow-xl overflow-hidden"}
+                className={"w-fit h-fit bg-white absolute rounded-md shadow-xl"}
                 key="filter_address_list"
                 style={{ zIndex: 10001, top: 200, right:0}}
             >
-                <div className="w-80 h-40 bg-white p-1">
-                    <div className="">
-                        Có {props.addressList?.list?.length} {props.addressList?.type} gần đây:
+                <div className="w-80 h-40 bg-white p-1 overflow-scroll h-96">
+                    <div className="text-lg p-2 font-semibold">
+                        Có {props.addressList?.list?.length} {props.addressList?.type} xung quanh:
                     </div>
                     <div>
                     {
