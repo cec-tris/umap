28e7ee402c19dc5296f1724d2f7b49cb72348577
lostmymind-18 <supercool1170@gmail.com>
2023-07-25 13:50:58 +0700

    abc

--- umap-web/src/components/MapTools/MapInteraction/FiterListAddresses/AddressItem.tsx
+++ umap-web/src/components/MapTools/MapInteraction/FiterListAddresses/AddressItem.tsx
@@ -3,11 +3,11 @@ type AddressItem = {address:string};
 export default function AddressItem(props:AddressItem){
     const handle=()=>{console.log("On click...")};
     return (
-        <div className="border-2 border-black shadow-sm my-1 px-1 ">
+        <div className="shadow-md my-1 px-1 ">
             <div>
                 {props.address}
             </div>
-            <div className="italic text-lime-500 hover:cursor-pointer" onClick={handle}>
+            <div className="italic text-lime-500 text-sm hover:cursor-pointer" onClick={handle}>
                 Chỉ đường &#62;&#62;&#62;
             </div>
         </div>
--- umap-web/src/components/MapTools/MapInteraction/FiterListAddresses/FilterListAddresses.tsx
+++ umap-web/src/components/MapTools/MapInteraction/FiterListAddresses/FilterListAddresses.tsx
@@ -8,6 +8,9 @@ interface FilterListAddressesProps{
 }
 
 export default function FilterListAddresses(props:FilterListAddressesProps){
+    const closeHandler = ()=>{
+        props.setShowFilterList(false);
+    }
     props.addressList.type = "cafe";
     props.addressList.list = ["Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học",
     "Cà phê Yes, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học","Cà phê Xyz, đường Trần Mạnh Dũng, làng Đại học",
@@ -25,6 +28,17 @@ export default function FilterListAddresses(props:FilterListAddressesProps){
                 style={{ zIndex: 10001, top: 200, right:0}}
             >
                 <div className="w-80 h-40 bg-white p-1 overflow-scroll h-96">
+                    <div className="w-full h-10 bg-white p-1">
+                        {/* <CloseButton style={{float:'right',
+                            backgroundSize:"cover", 
+                            width:"40px", height:"40px"}}></CloseButton> */}
+                        <button onClick={closeHandler} type="button" className="hover:bg-neutral-200"
+                            aria-label="Close" style={{ float: 'right' }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" fill="currentColor" className="bi bi-x " viewBox="0 0 16 16">
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                            </svg>
+                        </button>
+                    </div>
                     <div className="text-lg p-2 font-semibold">
                         Có {props.addressList?.list?.length} {props.addressList?.type} xung quanh:
                     </div>
